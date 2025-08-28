d6304c3681a2d07f8c0ae7900351b33cd93e1c41
Segun <sogbesansegun22@gmail.com>
2025-08-28 17:43:08 +0100

    Chore: scaffold app with React, Typescript, ESLint, and Prettier

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import '~/styles/global.css';
+
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen">
+        Hello World
+      </div>
+    </>
+  );
+}
+
+export default App;
